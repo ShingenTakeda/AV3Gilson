58da553f7c0e0dad3e06ebb0c8dab89255f57dab
Gustavo Takeda <gusmitsuo@gmail.com>
2022-12-10 15:48:24 -0300

    Cut stuff from class_def

--- server.ts
+++ server.ts
@@ -2,10 +2,15 @@
 import { Application } from "https://deno.land/x/oak@v10.5.1/mod.ts";
 import { viewEngine, oakAdapter , etaEngine} from "https://deno.land/x/view_engine@v10.5.1c/mod.ts"
 import { Test } from "./js/entry.js"
+import { Selecao } from "./js/class_defs.js";
 
 const app = new Application();
 
 Test();
+
+const brasil = new Selecao("brasil", "G", 0, 0, 0);
+
+console.log(brasil.grupo);
 
 app.use(
   viewEngine(oakAdapter, etaEngine, {
